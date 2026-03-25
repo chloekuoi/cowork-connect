1e01e9ac3b3bca3a99c72617afe327cfbff0308a
Chloe Guo <chloe.kuoi.g@gmail.com>
2026-03-25 16:39:33 -0400

    feat: load DM Sans fonts for Clover rebrand

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { AuthProvider } from './src/context/AuthContext';
 import RootNavigator from './src/navigation';
 import * as SplashScreen from 'expo-splash-screen';
 import { useFonts } from 'expo-font';
+import { DMSans_300Light, DMSans_400Regular, DMSans_500Medium } from '@expo-google-fonts/dm-sans';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -16,6 +17,9 @@ export default function App() {
     'Inter-Light': require('./assets/fonts/Inter-Light.ttf'),
     'Inter-Regular': require('./assets/fonts/Inter-Regular.ttf'),
     'Inter-Medium': require('./assets/fonts/Inter-Medium.ttf'),
+    DMSans_300Light,
+    DMSans_400Regular,
+    DMSans_500Medium,
   });
 
   useEffect(() => {
